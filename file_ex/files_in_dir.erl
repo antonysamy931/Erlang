4feff32edcec8722ae12690931ad46d75617684b
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2016-06-28 16:47:56 +0530

    File path dynamically generated

--- file_ex/files_in_dir.erl
+++ file_ex/files_in_dir.erl
@@ -4,15 +4,21 @@
 read_dir_files(Directory) ->
     case file:list_dir(Directory) of
         {ok, Filenames} ->
-            lists:foreach(fun(Name) ->
-                io:format("~s~n",[Name])                
-                % read_file_information(Name)
+            lists:foreach(fun(Name) ->      
+                File_Path = display(Directory, Name),          
+                % io:format("~s~n",[File_Path])                
+                read_file_information(File_Path)
             end, Filenames);
         {error, enoent} ->
             io:format("Directory not exist~n",[]);
         {error, Reason} ->
             io:format("~s~n",[Reason])
     end.
+
+display(Directory,FileName) ->    
+    File_Name = lists:flatten(io_lib:format("~s",[FileName])),
+    Directory_Name = lists:flatten(io_lib:format("~s",[Directory])),
+    Directory_Name++"/"++File_Name.
 
 read_file_information(FileName) ->
     case file:read_file_info(FileName) of
