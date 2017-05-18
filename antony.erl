1fbe37f833f8d48b8aa3dc5769f9613bcdc440d0
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2017-05-18 20:44:28 +0530

    Distributed Programming research

--- antony.erl
+++ antony.erl
@@ -1,5 +1,9 @@
 -module(antony).
-
+-import(io,[format/1,format/2]).
+-import(atomstut,[converts/2]).
 -export([display/0]).
 
-display() -> io:format("Antony create first erlang program module~n").
+display() ->     
+    X = converts(10,inch),
+    format("~w~n",[X]),
+    format("Antony create first erlang program module~n").    
